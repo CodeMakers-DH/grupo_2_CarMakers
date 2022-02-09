738d8cf3145b0680021da97a5b3f56b338de46b9
Sara Valentina Latyn <latynsara41@gmail.com>
2022-02-09 14:56:23 -0300

    Agregué carpetas y completé el app.js, creé el html y el css

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const exports = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
 
-app.listen(5000, ()=>{
-    console.log("Servidor para CarMakers")
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(5000, () => {
+    console.log('Servidor para CarMakers');
 })
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'))
+})
+
