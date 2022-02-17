2dc6c0c34fa4c7c4240bb0e086ead77965d18f14
Sara Valentina Latyn <latynsara41@gmail.com>
2022-02-17 17:01:30 -0300

    Unifiqué hojas de estilo del login

--- app.js
+++ app.js
@@ -22,3 +22,7 @@ app.get('/carrito', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/carrito.html'))
 })
 
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
