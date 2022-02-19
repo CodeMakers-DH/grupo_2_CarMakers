cf8c72c1cd3f473d348e180a28bc7ead53f8e500
Sara Valentina Latyn <latynsara41@gmail.com>
2022-02-18 22:41:18 -0300

    Backup

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,12 @@
 const menu = document.querySelector('.menu');
+const cruz = document.querySelector('.cruz');
 const enlaces = document.querySelector('.enlaces-menu');
 
 menu.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
 });
+
+cruz.addEventListener('click', () => {
+    enlaces.classList.toggle('desactivado');
+})
 
