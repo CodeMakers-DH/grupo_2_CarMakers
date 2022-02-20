539c39ff3cbd872e3aded7b6e104b6a63e7fc122
Sara Valentina Latyn <latynsara41@gmail.com>
2022-02-20 17:18:09 -0300

    Modifique el main para tablet y desktop

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,15 @@ const menu = document.querySelector('.menu');
 const cruz = document.querySelector('.cruz');
 const enlaces = document.querySelector('.enlaces-menu');
 
+
+
 menu.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
+    cruz.classList.toggle('activado');
 });
 
 cruz.addEventListener('click', () => {
-    enlaces.classList.toggle('desactivado');
+    enlaces.classList.toggle('activado');
+    cruz.classList.toggle('activado');
 })
 
