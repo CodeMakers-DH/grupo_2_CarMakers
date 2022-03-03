06a71c72c18d9452c39a72a0b3eaea33fb8e1ae6
Sara Valentina Latyn <latynsara41@gmail.com>
2022-03-03 10:04:58 -0300

    Backup para modificar el index

--- src/app.js
+++ src/app.js
@@ -14,6 +14,6 @@ app.set('views', path.join(__dirname,'/views'))
 app.use(mainRoutes);
 
 // llamada al servidor en puerto 5000
-app.listen(5001, () => {
+app.listen(5003, () => {
     console.log('Servidor para CarMakers');
 })
