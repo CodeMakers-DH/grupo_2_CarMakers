8f75ad9a5c280c6daa0127248cd0528fbff0b658
Sara Valentina Latyn <latynsara41@gmail.com>
2022-03-14 15:44:47 -0300

    agregué la vista productos

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const path = require('path');
-
+const multer = require('multer');
+const router = express.Router();
 const productsController = require('../controllers/productController');
 
-const router = express.Router();
+
+/*** USAMOS EL MULTER ***/ 
+const storage = multer.diskStorage({
+    destination: (req,file, cb) =>{
+        cb (null, 'public/images/products')
+    },
+    filename: (req,file,cb) =>{
+        console.log(file);
+        const nuevoNombre = 'nombre_imagen' + Date.now() + path.extname(file.originalname);
+        cb (null, nuevoNombre);
+        console.log (nuevoNombre);
+    }
+})
+const upload = multer ({storage})
 
 
 //rutas de las vistas
 
-//vista detalle de producto
+//detalle de producto
 router.get('/detalleproducto/:idModelo?', productsController.detalleProducto);
 
-//vista crear Producto
+//crear Producto
 router.get('/crearproducto', productsController.crearproducto);
+router.post('/products/crearproducto', upload.single('imgProducto'), productsController.create);
 
-//vista editar Producto
+
+//editar Producto
 router.get('/editarproducto', productsController.editarproducto);
 
+
+//inventario
+router.get('/', productsController.products);
 
 
 
