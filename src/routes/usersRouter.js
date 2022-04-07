07ae09781da73d189033c928b0e8f033d4d111a1
saravalentinal <latynsara41@gmail.com>
2022-04-07 21:47:23 +0200

    Multer para la img de perfil

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const path = require('path');
-// const multer = require('multer'); 
+const multer = require('multer'); 
 const router = express.Router();
 const usersController = require('../controllers/usersController');
 
-
+//Usamos el Multer
+const storage = multer.diskStorage({
+    destination: (req,file, cb) =>{
+        cb (null, 'public/imgs/imgsPerfil')
+    },
+    filename: (req,file,cb) =>{
+        console.log(file);
+        const nuevoNombre = 'nombre_imagen' + Date.now() + path.extname(file.originalname);
+        cb (null, nuevoNombre);
+        console.log (nuevoNombre);
+    }
+})
+const upload = multer ({storage})
 
 //rutas de las vistas
 router.get('/', usersController.index);
@@ -13,7 +25,19 @@ router.get('/detalleUsuario/:idUsuario?', usersController.detalleUsuario);
 
 //crear usuario
 router.get('/registro', usersController.registro);
-router.post('/', usersController.crear);
+router.post('/', upload.single('imgPerfil'), (req, res, next) => {
+    const file = req.file;
+    if(!file){
+        const error = new Error('Por favor seleccione un archivo')
+        error.htttpStatusCode = 400;
+        return next(error);
+    } else if(path.extname(file.originalname) != ".jpg" && path.extname(file.originalname) != ".jpeg" && path.extname(file.originalname) != ".png"){
+        const error = new Error('Por favor seleccione un archivo de formato JPG, JPEG o PNG.');
+        error.htttpStatusCode = 400;
+        return next(error)
+    }
+    next();
+    }, usersController.crear);
 
 //editar usuario
 router.get('/editarUsuario/:idUsuario?', usersController.editarUsuario);
