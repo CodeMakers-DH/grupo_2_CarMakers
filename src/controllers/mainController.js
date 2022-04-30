2427157f2655bb3ac67c89024c3e617fe24508a4
Cristian Patricio Salinas Talamilla <ctalamilla@MacBook-Pro-de-Cristian.local>
2022-04-30 15:32:49 -0300

    index funcionando con squelize

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const fs = require('fs');
-const productsFilePath = path.join(__dirname, '../data/productos.json');
-const productosParseados = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+//const productsFilePath = path.join(__dirname, '../data/productos.json');
+//const productosParseados = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const db = require("../../database/models")
+
 
 //controladores de vitas
 const controlador ={
     index:(req, res) => {
-        res.render('index', {productosParseados})
+        db.Producto.findAll()
+            .then(productos=> res.render('index', {"productosParseados":productos}) )
+        //res.render('index', {productosParseados})
     },
     carritoCompras: (req, res) => {
         res.render('carrito')
