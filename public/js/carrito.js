8b5fe04b2d69698977c4fbe81b9fb793d3b0c748
Cristian Patricio Salinas Talamilla <ctalamilla@MacBook-Pro-de-Cristian.local>
2022-06-11 19:30:13 -0300

    carrito andasndo en front

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -7,6 +7,7 @@ let idModelSelected = []
 for (let i in carrito) {
     idModelSelected.push(carrito[i].id)}
 
+//console.log(carrito)
 
 const res = await fetch('http://localhost:3000/api/products');
 const productsJSON = await res.json();
@@ -15,16 +16,41 @@ var elementSelected = []
 for (let index = 0; index < idModelSelected.length; index++) {
     elementSelected.push(products.find(elem => elem.idModelo ==idModelSelected[index]))
     }
+//console.log(elementSelected)
+
+for(let i  in elementSelected){
+    //console.log(elementSelected[i])
+    for (let j in carrito){
+        //console.log(carrito[j])
+        if(elementSelected[i].idModelo == carrito[j].id){
+            elementSelected[i]['count']= carrito[j].count
+
+        }
+
+    }
+}
 console.log(elementSelected)
 
+elementSelected['total'] = 0
+elementSelected.map(elemento => {
+
+    elemento['subtotal']= elemento.count * elemento.precio
+    elementSelected['total']+= elemento.subtotal
+})
+
+
+
+
+console.log(elementSelected)
 
 const htmlString = elementSelected
         .map((product) => {
             return `
             <li>
                 <h3 href="/products/detail/${product.idModelo}" class="inv-nombre">${product.nombreModelo}</h3>
-                
                 <h4 class="inv-precio">USD$ ${product.precio}.000.000</h4>
+                <h4 class="inv-precio"> ${product.count} unidades</h4>
+                <h4 class="inv-precio">Subtotal $ ${product.precio * product.count}.000.000 </h4>
             </li>
         `;
         })
@@ -46,3 +72,13 @@ vaciarCarrito.addEventListener('click', function(event){
     event.preventDefault()
 
 });
+
+const total = document.getElementById('total-compra'
+)
+
+total.innerHTML = `$ ${elementSelected.total}.000.000`
+
+
+
+
+
