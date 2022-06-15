f5b1f66832a4ca4e80d56736a45de2df5105bce6
saravalentinal <latynsara41@gmail.com>
2022-06-15 22:44:51 +0200

    cambios css editar producto

---
+++ src/midddlewares/validateUserToEdit.js
@@ -0,0 +1,29 @@
+const {check} = require('express-validator');
+
+
+const validateUserToEdit = [
+    check('nombres')
+        .notEmpty().withMessage('Debes rellenar los nombres').bail()
+        .isLength({min:2}).withMessage('El nombre debe tener al menos 2 caracteres.').bail(),
+    check('apellidos')
+        .notEmpty().withMessage('Debes rellenar los apellidos').bail()
+        .isLength({min:2}).withMessage('El apellido debe tener al menos 2 caracteres.').bail(),
+    check('email')
+        .notEmpty().withMessage('Debes rellenar el email').bail()
+        .isEmail().withMessage('Ingresa un email de formato válido.').bail(),
+    check('password')
+        .notEmpty().withMessage('Debes rellenar la contraseña').bail()
+        .isLength({min:8}).withMessage('La contraseña debe tener al menos 8 caracteres.').bail()
+        .custom((value, {req}) => {
+            let pass = req.body.password;
+
+            if(pass.match(/[0-9]/) && pass.match(/[A-Z]/) && pass.match(/[a-z]/) && pass.match(/[.,:;-_!"§$%&/()=?`+@]/)){
+                return true;
+            } 
+            else{
+            throw new Error('La contraseña debe tener al menos un número, una mayúscula, minúscula y un caracter especial.')
+        }
+        })
+];
+
+module.exports = validateUserToEdit;
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -9,6 +9,8 @@ const guestMiddleware = require('../midddlewares/guestMiddleware');
 const authMiddleware = require('../midddlewares/authMiddleware');
 const adminMiddleware = require('../midddlewares/adminMiddleware');
 
+const validateUserToEdit = require('../midddlewares/validateUserToEdit');
+
 
 //Usamos el Multer
 const storage = multer.diskStorage({
@@ -101,33 +103,6 @@ const validateRegister = [
             return true;
         }),
 ];
-
-const validateUserToEdit = [
-    check('nombres')
-        .notEmpty().withMessage('Debes rellenar los nombres').bail()
-        .isLength({min:2}).withMessage('El nombre debe tener al menos 2 caracteres.').bail(),
-    check('apellidos')
-        .notEmpty().withMessage('Debes rellenar los apellidos').bail()
-        .isLength({min:2}).withMessage('El apellido debe tener al menos 2 caracteres.').bail(),
-    check('email')
-        .notEmpty().withMessage('Debes rellenar el email').bail()
-        .isEmail().withMessage('Ingresa un email de formato válido.').bail(),
-    check('password')
-        .notEmpty().withMessage('Debes rellenar la contraseña').bail()
-        .isLength({min:8}).withMessage('La contraseña debe tener al menos 8 caracteres.').bail()
-        .custom((value, {req}) => {
-            let pass = req.body.password;
-
-            if(pass.match(/[0-9]/) && pass.match(/[A-Z]/) && pass.match(/[a-z]/) && pass.match(/[.,:;-_!"§$%&/()=?`+@]/)){
-                return true;
-            } 
-            else{
-            throw new Error('La contraseña debe tener al menos un número, una mayúscula, minúscula y un caracter especial.')
-        }
-        })
-];
-
-
 
 
 router.post('/register', upload.single('imgPerfil'), validateRegister, usersController.processRegister);
